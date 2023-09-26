a467092709bc20ce9e61185cac1b3417cf7e3fe5
Miguel Negron <manegron@redborder.com>
2023-09-26 22:38:17 +0100

    Match metadata version with tag

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'vimesa@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook-k2http'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
